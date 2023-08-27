7d43fe9d2f3d2525d86e270a42cf3d5e97280fa6
jaminFisher <jaminFisher@gmail.com>
2023-08-27 16:11:20 +0100

    updated home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,19 +33,90 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "jaminfisher";
+    homeDirectory = "/home/jaminfisher";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.starship
+    pkgs.zsh
+    pkgs.htop
+    pkgs.micro
+    pkgs.vscode
+    pkgs.alacritty
+    pkgs.gitui
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  fonts.fontconfig.enable = true;
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+      "/.config/starship.toml".source = ~/dotfiles/starship.toml;
+      ".bashrc".source = ~/dotfiles/.bashrc;
+      ".zshrc".source = ~/dotfiles/.zshrc;
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source      ".bashrc"
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/jaminfisher/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    EDITOR = "micro";
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font.normal = {
+        family = "FiraCode Nerd Font";
+        style = "Regular";          
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Ben Fisher";
+    userEmail = "jaminFisher@gmail.com";
+  };
+
+  programs.git-credential-oauth.enable = true;
+  
+  programs.zsh.enable = true;
+  programs.starship.enable = true;
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
