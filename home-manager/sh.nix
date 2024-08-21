451366641189858ecf61655a80bd5a92de7369e5
Ben Fisher <jaminfisher@gmail.com>
2024-08-21 13:54:01 +0100

    fix

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -106,16 +106,9 @@ in
 
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
-    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
     enableCompletion = true;
     shellAliases = myAliases;
-    initExtra = ''
-    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
-     %F{green}→%f "
-    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
-    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
-    '';
   };
 
   programs.bash = {
