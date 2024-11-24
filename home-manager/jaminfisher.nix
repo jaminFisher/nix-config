67e4660780733a214281720963f71bfa6e4e9758
Ben Fisher <jaminfisher@gmail.com>
2024-11-24 20:55:00 +0000

    added helix config to jaminfisher.nix

--- home-manager/jaminfisher.nix
+++ home-manager/jaminfisher.nix
@@ -40,6 +40,29 @@
     userEmail = "jaminFisher@gmail.com";
   };
 
+programs.helix = {
+  enable = true;
+  settings = {
+    theme = "autumn_night_transparent";
+    editor.cursor-shape = {
+      normal = "block";
+      insert = "bar";
+      select = "underline";
+    };
+  };
+  languages.language = [{
+    name = "nix";
+    auto-format = true;
+    formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+  }];
+  themes = {
+    autumn_night_transparent = {
+      "inherits" = "autumn_night";
+      "ui.background" = { };
+    };
+  };
+};
+
   # Enable home-manager and git
   programs.home-manager.enable = true;
 
