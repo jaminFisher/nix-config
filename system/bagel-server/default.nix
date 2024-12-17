e7364bf63897944cb2aa889a58c75cd339363665
Ben Fisher <jaminFisher@gmail.com>
2024-12-17 13:15:40 +0000

    added Hardware Transcoding options to support jellyfin

--- system/bagel-server/default.nix
+++ system/bagel-server/default.nix
@@ -17,12 +17,30 @@
   networking.networkmanager.enable = true;
   services.tailscale.enable = true;
   services.vaultwarden.enable = true;
-  services.jellyfin.enable = true;
+
+  # Jellyfin
+
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
     pkgs.jellyfin-ffmpeg
   ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+  services.jellyfin.enable = true;
 
   home-manager = {
     extraSpecialArgs = {
