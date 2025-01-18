95481d1d6bae53bd48a7cd1c01b96b364aafa2a7
Ben Fisher <jaminFisher@gmail.com>
2025-01-18 20:30:48 +0000

    gnome.nix: adding keymap to xserver to fix issues at login, adding gpaste

--- system/x220/gnome.nix
+++ system/x220/gnome.nix
@@ -1,9 +1,12 @@
-{...}:
-{
+{ ... }: {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
+  services.xserver = {
+    enable = true;
+    xkb.layout = "gb";
+  };
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  programs.gpaste.enable = true;
 }
