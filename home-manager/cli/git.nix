12bb56e919fe6a0c42cf2bd4c0e42b5fdc34ca3c
Ben Fisher <jaminFisher@gmail.com>
2025-02-04 11:06:33 +0000

    added some git aliases

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -9,6 +9,13 @@
       background = "dark";
       display = "side-by-side-show-both";
     };
+    aliases = {
+      co = "checkout";
+      pc = "!git push origin '$(git symbolic-ref --short HEAD)'";
+      undo = "reset HEAD~1 --mixed";
+      amend = "commit --amend --no-edit";
+      br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+    };
     # extraConfig = {
     #   credential.helper = "cache";
     # };
