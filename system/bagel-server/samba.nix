7f369dbcb6ad2723816d02de22bafc2eacaf273c
Ben Fisher <jaminFisher@gmail.com>
2025-08-27 14:06:29 +0100

    Added Seagate to Smb share

--- system/bagel-server/samba.nix
+++ system/bagel-server/samba.nix
@@ -33,6 +33,14 @@
         "create mask" = "0777";
         "directory mask" = "0777";
       };
+      "Seagate" = {
+        "path" = "/mnt/Seagate";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0777";
+        "directory mask" = "0777";
+      };
       # "private" = {
       #   "path" = "/mnt/Shares/Private";
       #   "browseable" = "yes";
