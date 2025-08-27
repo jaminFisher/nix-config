1cd9547bcacc4451182328f22a41a473f0be9f8f
Ben Fisher <jaminFisher@gmail.com>
2025-08-27 14:39:17 +0100

    changed from unstable to 25.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
